64229c3eeff62260062f101d7e79c78718fc1522
gioambro <gioambro03@gmail.com>
2025-09-13 23:46:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from punteggi import calcola_punteggio
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Benvenuto su Fanta Formula 1! 🚦🏎️"
+    return render_template("home.html")
+
+@app.route("/calcola", methods=["POST"])
+def calcola():
+    risultato = {
+        "posizione": int(request.form["posizione"]),
+        "sprint": "sprint" in request.form,
+        "pole": "pole" in request.form,
+        "dnf": "dnf" in request.form
+    }
+    punti = calcola_punteggio(risultato)
+    return render_template("risultato.html", punti=punti)
 
 if __name__ == "__main__":
-    # Importante: host 0.0.0.0 per Render, porta 10000
     app.run(host="0.0.0.0", port=10000, debug=False)
